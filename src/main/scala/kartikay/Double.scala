00873c7fc480b743dca33623fd2bfd963185f3d9
kartikay3813 <kartikay.bhardwaj@knoldus.com>
2021-12-09 15:15:55 +0530

    Oops assignment 1

---
+++ src/main/scala/kartikay/Double.scala
@@ -0,0 +1,20 @@
+package kartikay
+import scala.collection.mutable.ListBuffer
+
+class DoubleQueue(q : ListBuffer[Int]) extends Queue {
+
+  override def enqueue(pushedElement: Int): Unit = {
+
+    q += (pushedElement * 2)
+  }
+
+}
+
+class SquareQueue(q: ListBuffer[Int]) extends Queue {
+
+  override def enqueue(pushedElement: Int): Unit = {
+
+    q += (pushedElement * pushedElement)
+  }
+
+}
